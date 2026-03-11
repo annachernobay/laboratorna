792abb297f571ba6672409468c94ec3faeba1158
Ганна Чернобай <anna.chernobay2008@gmail.com>
2026-03-11 08:02:05 +0200

    add core functionality to Book class

--- Book.cpp
+++ Book.cpp
@@ -1,1 +1,13 @@
 #include "Book.h"
+
+Book::Book(string t, string a, int y) : title(t), author(a), year(y) {
+    cout << "Constructor created" << endl;
+}
+
+Book::~Book() {
+    cout << "Destructor created" << endl;
+}
+
+void Book::getInfo() {
+    cout << "Book: " << title << " | Author: " << author << " | Year: " << year << endl;
+}
--- Book.h
+++ Book.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <string>
+#include <iostream>
+using namespace std;
 class Book
 {
+    private:
+        string title;
+        string author;
+        int year;
+
+    public:
+        Book(string t = "Untitled", string a = "Unknown", int y = 2026);
+        ~Book();
+        void getInfo();
+
 };
+
 
