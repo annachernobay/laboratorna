79b9518ad39141ed738cfb27b478ac375a8df6d3
Ганна Чернобай <anna.chernobay2008@gmail.com>
2026-03-11 16:59:26 +0200

    add core functionality to Reader class

--- Reader.cpp
+++ Reader.cpp
@@ -1,1 +1,15 @@
 #include "Reader.h"
+
+Reader::Reader(string n, int i, int c) : name(n), id(i), booksCount(c) {
+    cout << "Constructor created" << endl;
+}
+
+Reader::Reader(string n, int i) : Reader(n, i, 0) {}
+
+Reader::~Reader() {
+    cout << "Destructor created" << endl;
+}
+
+void Reader::getInfo() {
+    cout << "Reader: " << name << " | ID: " << id << " | Books: " << booksCount << endl;
+}
--- Reader.h
+++ Reader.h
@@ -1,5 +1,17 @@
 #pragma once
-class Reader
-{
-};
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Reader {
+private:
+    string name;
+    int id;
+    int booksCount;
+
+public:
+    Reader(string n = "Anonim", int i = 0, int c = 0);
+    Reader(string n, int i);
+    ~Reader();
+    void getInfo();
+};
